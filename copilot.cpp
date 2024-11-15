c79b8eb9694f570253f1b61a2390b02f735d6819
ratitimora <ratitimora@gmail.com>
2024-11-15 13:33:52 -0800

    Add files via upload

---
+++ copilot.cpp
@@ -0,0 +1,29 @@
+#include <google-maps-device-locator.h>
+//#include <carloop.h>
+#include "Particle.h"
+#include "cellular_hal.h"
+
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+GoogleMapsDeviceLocator locator;
+
+STARTUP(cellular_credentials_set("hologram", "", "", NULL));
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+ApplicationWatchdog wd(1800000, System.reset);
+
+void locationCallback(float lat, float lon, float accuracy)
+{
+    Log.info("callback");
+    Particle.publish("googlemaps-data", "{ \"device\": \"" + String(System.deviceID()) + "\", \"lat\":\"" + String(lat) + "\", \"lon\":\"" + String(lon) + "\", \"satellites\":\"" + String(accuracy), PRIVATE);    
+}
+
+void setup()
+{
+    locator.withSubscribe(locationCallback).withLocatePeriodic(300);
+}
+
+void loop()
+{
+    locator.loop();
+}
